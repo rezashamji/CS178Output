867ad80ff98874eaeb817ba2236220919e2aa9c6
NITHYA GOTTIPATI <nithyagottipati@college.harvard.edu>
2024-03-18 18:28:20 -0400

    fixed app7

--- app7.py
+++ app7.py
@@ -78,7 +78,7 @@ def index():
     stop_names = [stop['stop_name'] for stop in stops_data]
 
     # Pass shapes_data to the template
-    return render_template('index7.html', map=map_html, stop_names=stop_names, shapes_data=shapes_data)
+    return render_template('index7.html', map=map_html, stop_names=stop_names, shapes_data=shapes_data, stops_data=stops_data)
 
 
 # Route to serve live bus data
